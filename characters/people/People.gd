f4af56fd555ad598ae53c2b3c5d89633996d215b
Eric Marty <contact@ericmarty.co>
2019-09-29 19:01:26 -0700

    Add input test function and finish mapping controls

--- characters/people/People.gd
+++ characters/people/People.gd
@@ -60,9 +60,94 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
-
+	_input_tests()
 	velocity = move_and_slide(velocity, UP)
 
+func _input_tests():
+	# Brake
+	if Input.is_action_pressed("brake"):
+		print("brake")
+		
+	# Camera In
+	if Input.is_action_pressed("camera_in"):
+		print("camera_in")
+	
+	# Camera Out
+	if Input.is_action_pressed("camera_out"):
+		print("camera_out")
+		
+	# Camera Reset
+	if Input.is_action_pressed("camera_reset"):
+		print("camera_reset")
+		
+	# Goto Marker
+	if Input.is_action_pressed("goto_marker"):
+		print("goto_marker")
+		
+	# Grab Left
+	if Input.is_action_pressed("grab_left"):
+		print("grab_left")
+	
+	# Grab Right
+	if Input.is_action_pressed("grab_right"):
+		print("grab_right")
+		
+	# Place
+	if Input.is_action_pressed("place"):
+		print("place")
+		
+	# Place Marker
+	if Input.is_action_pressed("place_marker"):
+		print("place_marker")
+		
+	# Push Left
+	if Input.is_action_pressed("push_left"):
+		print("push_left")
+		
+	# Push Right
+	if Input.is_action_pressed("push_right"):
+		print("push_right")
+		
+	# Steer Left
+	if Input.is_action_pressed("steer_left"):
+		print("steer_left")
+		
+	# Steer Right
+	if Input.is_action_pressed("steer_right"):
+		print("steer_right")
+		
+	# Steer Up
+	if Input.is_action_pressed("steer_up"):
+		print("steer_up")
+		
+	# Steer Down
+	if Input.is_action_pressed("steer_down"):
+		print("steer_down")
+		
+	# Trick Down
+	if Input.is_action_pressed("trick_down"):
+		print("trick_down")
+		
+	# Trick Up
+	if Input.is_action_pressed("trick_up"):
+		print("trick_up")
+		
+	# Trick Left
+	if Input.is_action_pressed("trick_left"):
+		print("trick_left")
+		
+	# Trick Right
+	if Input.is_action_pressed("trick_right"):
+		print("trick_right")
+		
+	# Start
+	if Input.is_action_pressed("start"):
+		print("start")
+		
+	# Select
+	if Input.is_action_pressed("select"):
+		print("select")
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
